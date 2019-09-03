071ccb851536f1cb2c5036a592a41d3016db5260
suaas21 <sagor@appscode.com>
2019-09-03 09:49:27 +0600

    Fix

--- pkg/create.go
+++ pkg/create.go
@@ -5,65 +5,14 @@ import (
 	"github.com/spf13/cobra"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/client-go/kubernetes"
-	"stash.appscode.dev/stash/apis/stash/v1alpha1"
-	"stash.appscode.dev/stash/apis/stash/v1beta1"
 	cs "stash.appscode.dev/stash/client/clientset/versioned"
-	core "k8s.io/api/core/v1"
 )
 
 var (
 	namespace   string
 	kubeClient  *kubernetes.Clientset
 	stashClient *cs.Clientset
-
-	opt                 = option{}
-	volumeMounts        []core.VolumeMount
-	paths               []string
-	snapshots            []string
-	volumeClaimTemplate core.PersistentVolumeClaim
 )
-
-type option struct {
-	// For Repository
-	Provider       string
-	Bucket         string
-	Endpoint       string
-	URL            string
-	MaxConnections int
-	Secret         string
-	Prefix         string
-	Container      string
-    // For BackupConfiguration && RestoreSession
-	Paths     string
-	VolMounts string
-
-	TargetRef       v1beta1.TargetRef
-	RetentionPolicy v1alpha1.RetentionPolicy
-	RepositoryName  string
-	Schedule        string
-	Driver          string
-	VSClassName     string
-	TaskName        string
-	Replica         int32
-	Pause           bool
-
-	Rule            rule
-	VolumeClaimTemp volumeclaimTemp
-}
-
-type volumeclaimTemp struct {
-	Name             string
-	AccessMode       string
-	StorageClassName string
-	Size             string
-	DataSourceName   string
-}
-
-type rule struct {
-	SourceHost string
-	Snapshot  string
-	Paths      string
-}
 
 func NewCmdCreate(clientGetter genericclioptions.RESTClientGetter) *cobra.Command {
 	cmd := &cobra.Command{
